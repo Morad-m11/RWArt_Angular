7c5f0d055d185422161b235f796c48d495227997
Angela <angela.hanko@gmail.com>
2025-06-21 19:03:50 +0200

    feat: logout/redirect on token expiration (401 in interceptor)

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,14 +2,28 @@ import { inject, Injectable } from '@angular/core';
 import { User } from './user/login.component';
 import { HttpClient } from '@angular/common/http';
 import { firstValueFrom } from 'rxjs';
+import { Router } from '@angular/router';
+import { SnackbarService } from '../core/snackbar/snackbar.service';
 
 @Injectable({
    providedIn: 'root'
 })
 export class AuthService {
    private readonly _http = inject(HttpClient);
+   private readonly _router = inject(Router);
+   private readonly _snackbar = inject(SnackbarService);
 
    async login(user: User) {
       await firstValueFrom(this._http.post('http://localhost:3000/auth/login', user));
+   }
+
+   logout({ expired } = { expired: false }) {
+      if (expired) {
+         this._snackbar.error('Session expired');
+      } else {
+         this._http.post('http://localhost:3000/auth/logout', null);
+      }
+
+      this._router.navigateByUrl('login');
    }
 }
--- src/app/core/auth-interceptor.ts
+++ src/app/core/auth-interceptor.ts
@@ -1,6 +1,23 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import {
+   HttpErrorResponse,
+   HttpInterceptorFn,
+   HttpStatusCode
+} from '@angular/common/http';
+import { inject } from '@angular/core';
+import { catchError, throwError } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
+   const authService = inject(AuthService);
    const reqWithAuth = req.clone({ withCredentials: true });
-   return next(reqWithAuth);
+
+   return next(reqWithAuth).pipe(
+      catchError((err: HttpErrorResponse) => {
+         if (err.status === HttpStatusCode.Unauthorized) {
+            authService.logout({ expired: true });
+         }
+
+         return throwError(() => err);
+      })
+   );
 };
