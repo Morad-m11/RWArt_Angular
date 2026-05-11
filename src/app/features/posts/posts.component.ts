5498980d07d9f728b599dcd62b01e4b3b999c410
Mo <80397392+Morad-m11@users.noreply.github.com>
2026-05-11 20:17:26 +0200

    styled featured/posts tabs

--- src/app/features/posts/posts.component.ts
+++ src/app/features/posts/posts.component.ts
@@ -1,11 +1,20 @@
+import { Tab, TabContent, TabList, TabPanel, Tabs } from '@angular/aria/tabs';
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { MaterialModule } from 'src/app/shared/material.module';
+import { FeaturedComponent } from './components/featured/featured.component';
+import { PostListComponent } from './components/post-list/post-list.component';
 
 @Component({
     selector: 'app-posts',
     standalone: true,
-    imports: [MaterialModule, RouterModule],
+    imports: [
+        Tab,
+        Tabs,
+        TabList,
+        TabPanel,
+        TabContent,
+        FeaturedComponent,
+        PostListComponent
+    ],
     templateUrl: './posts.component.html',
     styleUrl: './posts.component.scss'
 })
